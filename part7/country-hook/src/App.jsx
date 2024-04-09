513601b553f61dd25ab515e7feec6e560e3645b4
ihssandk <ihssandoukali@gmail.com>
2024-04-09 23:36:35 +0000

    part7

---
+++ part7/country-hook/src/App.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+  return {type,value,onChange}
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    const fetchCountry = async () => {
+      try {
+        const countries = await axios.get(
+          'https://studies.cs.helsinki.fi/restcountries/api/all')
+          .then(res=>res.data)
+
+        if (countries.length > 0) {
+          const locateCountry = countries.filter(country => 
+            country.name.common.toLowerCase().includes(name.toLowerCase()))
+          setCountry(locateCountry)
+          console.log(locateCountry)
+        } else {
+          setCountry({ found: false })
+        }
+      } catch (error) {
+        console.error('Error fetching country:', error)
+      }
+    }
+    if (name) {
+      fetchCountry()
+    }
+  }, [name])
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found && !country) {
+    return <div>not found...</div>
+  }
+
+  return (
+    <div>
+      <h3>{country.name} </h3>
+      <div>capital {country.capital} </div>
+      <div>population {country.population}</div>
+      <img src={country.flag} height='100' alt={`flag of ${country.name}`} />
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+  
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
